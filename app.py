52958e284546f31379838af5132861651cf4fdc0
gustavoGarciaPereira <gusgurtavo@gmail.com>
2020-12-12 12:30:05 -0300

    testando layout

--- app.py
+++ app.py
@@ -5,13 +5,19 @@ import requests as r
 import json
 import os
 
+
 load_dotenv()
+
+
 app = Flask(__name__)
 CORS(app)
 
 @app.route('/')
 def gustavo():
-    return render_template('boas_vindas.html',nome=['gustavo','garcia','pereira'])
+    nomes = ['gustavo','garcia','pereira']
+    titulo = "Gustavo API"
+    return render_template('boas_vindas.html',
+                           nome=nomes,titulo=titulo)
 
 @app.route('/receita/')
 def hello_world():
@@ -41,7 +47,9 @@ def getGifs(lista_receitas):
 
 @app.route('/teste-templete/')
 def teste_template():
-    return render_template('conteudo.html')
+    return render_template('conteudo.html',
+                           imagem1 = "html-1.1s-47px(1).svg",
+                           imagem2 = "couch-1.1s-203px.svg")
 
 if __name__ == '__main__':
 	app.run(debug=True)
