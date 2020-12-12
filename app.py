95dc1ee6189960dd502e03d11b34fbf4d5513356
gustavoGarciaPereira <gusgurtavo@gmail.com>
2020-12-11 22:01:17 -0300

    ajuste no retorno do git

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ def getGifs(lista_receitas):
         t = r.request('GET',"https://api.giphy.com/v1/gifs/search?api_key={}&q='{}'&limit=1&offset=0&rating=g&lang=en".format(os.getenv("GIF_API"),lista_receitas[i]['title'])).text
     
         for j in json.loads(t)['data']:
-            lista_receitas[i]['url-gif'] = j['images']['480w_still']['url']
+            lista_receitas[i]['url-gif'] = j['images']['original']['url']
     return lista_receitas
     
 
