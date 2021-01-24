9237934c604578debc29de68bb848b6d29f250c7
gustavoGarciaPereira <gusgurtavo@gmail.com>
2021-01-24 18:36:05 -0300

    adicionado mais uma rota

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask_cors import CORS
 import requests as r
 import json
 import os
+import random
 
 
 load_dotenv()
@@ -61,6 +62,84 @@ def teste_template():
                         imagem2 = "couch-1.1s-203px.svg")
 
         
+
+@app.route('/api/ppt/jogada')
+def pedra_papel_tasoura():
+    usuario = request.args.get('jogada_usuario')
+    #computador = random.choice(['pedra','papel','tesoura'])
+    computador = random.choice(['tesoura'])
+    obj = vitoria(usuario, computador)
+    return jsonify(
+        obj
+    )
+
+def vitoria(usuario,computador):
+    obj = {
+        "usuario":"{}".format(usuario),
+        "computado":"{}".format(computador),
+        "vitoria":"",
+        "mensagem":"",
+        "img1":"",
+        "img2":""
+        }
+    img ={
+        'pedra':"https://res.cloudinary.com/dminw7meh/image/upload/v1611517829/img-jogo-site/pedra_nzefl3.svg",
+        'tesoura':"https://res.cloudinary.com/dminw7meh/image/upload/v1611517830/img-jogo-site/tesoura_oulwv9.svg",
+        'papel':"https://res.cloudinary.com/dminw7meh/image/upload/v1611517829/img-jogo-site/papel_rmwauk.svg",
+    }
+    # pe > t
+    # pe < pa
+    # t > pa
+
+    #https://res.cloudinary.com/dminw7meh/image/upload/v1611517829/img-jogo-site/pedra_nzefl3.svg
+    #https://res.cloudinary.com/dminw7meh/image/upload/v1611517830/img-jogo-site/tesoura_oulwv9.svg
+    #https://res.cloudinary.com/dminw7meh/image/upload/v1611517829/img-jogo-site/papel_rmwauk.svg
     
+    if usuario == 'pedra' and computador == 'tesoura':
+        obj['vitoria'] = 'usuario'
+        obj['mensagem'] = 'usuario venceu'
+        obj['img1'] = img[usuario]
+        obj['img2'] = img[computador]
+
+    if usuario == 'tesoura' and computador == 'pedra':
+        obj['vitoria'] = 'computador'
+        obj['mensagem'] = 'computador venceu'
+        obj['img2'] = img[computador] 
+        obj['img1'] = img[usuario]
+
+    if usuario == 'papel' and computador == 'pedra':
+        obj['vitoria'] = 'usuario'
+        obj['mensagem'] = 'usuario venceu'
+        obj['img1'] = img[usuario]
+        obj['img2'] = img[computador] 
+
+    if usuario == 'pedra' and computador == 'papel':
+        obj['vitoria'] = 'computador'
+        obj['mensagem'] = 'computador venceu'
+        obj['img2'] = img[computador] 
+        obj['img1'] = img[usuario]
+
+    if usuario == 'tesoura' and computador == 'papel':
+        obj['vitoria'] = 'usuario'
+        obj['mensagem'] = 'usuario venceu'
+        obj['img1'] = img[usuario]
+        obj['img2'] = img[computador] 
+
+    if usuario == 'papel' and computador == 'tesoura':
+        obj['vitoria'] = 'computador'
+        obj['mensagem'] = 'computador venceu'
+        obj['img2'] = img[computador] 
+        obj['img1'] = img[usuario]
+
+    if obj['vitoria']=='' and obj['mensagem'] == '':
+        obj['vitoria'] = 'empate'
+        obj['mensagem'] = 'deu empate'
+        print(usuario,"img[usuario]",img[usuario])
+        print(computador,"img[computador]",img[computador])
+        obj['img1'] = img[usuario]
+        obj['img2'] =  img[computador] 
+    
+    return obj
+
 if __name__ == '__main__':
-	app.run(debug=True)
+	app.run(debug=False)
